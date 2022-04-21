a8da1cb55ab376caba3139cb138bec43f6451452
Jan Hruby <hrubyjan.mt@gmail.com>
2022-04-21 19:11:02 +0200

    2002-04-21 - Xamarin project created

---
+++ GlobeStat/GlobeStat.App/GlobeStat.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GlobeStat.App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GlobeStat.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ GlobeStat/GlobeStat.App/GlobeStat.App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GlobeStat.App.Models;
+using GlobeStat.App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GlobeStat.App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
